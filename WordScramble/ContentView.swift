a11c0b94ef2bf88ebaa2283e39cc32fd750b004f
Peter Salz <psalz.app.dev@web.de>
2020-06-30 20:26:02 +0200

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Peter Salz on 30.06.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
