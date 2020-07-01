4f328d490b0f51fd6635da42e653c9b32c13a00c
Peter Salz <psalz.app.dev@web.de>
2020-07-01 08:42:16 +0200

    Set up app and basic UI

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,8 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            VStack {
+                TextField(
+                    "Enter your word",
+                    text: $newWord,
+                    onCommit: addNewWord
+                )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .autocapitalization(.none)
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+        }
+    }
+    
+    func addNewWord() {
+        
+        // Lowercase and trim the word, to make sure we don't add
+        // duplicate words with case differences.
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // Extra validation to come.
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame() {
+        
+        if let startWordsURL = Bundle.main.url(
+            forResource: "start",
+            withExtension: "txt"
+        ) {
+        
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
